29d337a0c083f318a281aaa879d8da1004fb2185
Pavel Rahman Taukir <paveltauker@gmail.com>
2023-09-21 15:34:15 +0600

    Main

--- Cyber-Tech-Screening-Test/tailwind.config.js
+++ Cyber-Tech-Screening-Test/tailwind.config.js
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4A3AFF",
+      },
+      backgroundImage: {
+        Hero: "url('assets/Hero.png')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
