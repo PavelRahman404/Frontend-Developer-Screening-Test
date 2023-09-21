352f1fdcdd1fa298e0144df7905c50b2cb74212f
Pavel Rahman Taukir <paveltauker@gmail.com>
2023-09-21 16:09:10 +0600

    Final

--- Cyber-Tech-Screening-Test/src/App.jsx
+++ Cyber-Tech-Screening-Test/src/App.jsx
@@ -10,8 +10,8 @@ const App = () => {
     >
       <Navbar />
       <div className="flex flex-col justify-center text-center items-center h-3/4">
-        <h2 className="text-white text-2xl font-medium ">Fashion Tips</h2>
-        <h1 className="md:text-5xl text-3xl text-white font-semibold py-5">
+        <h2 className="text-black text-2xl font-medium ">Fashion Tips</h2>
+        <h1 className="md:text-5xl text-3xl text-black font-semibold py-5">
           Items every woman should have
         </h1>
         <div className="text-xl">
